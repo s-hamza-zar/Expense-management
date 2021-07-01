0f9dc216737b1865ad8fc6e6532a74d9b5cd8883
s-hamza-zar <s.hamza.zar@gmail.com>
2021-07-01 23:57:33 +0500

    modal not opening

--- login.js
+++ login.js
@@ -1,35 +1,31 @@
 var email = document.querySelector("#emailId");
 var password = document.querySelector("#passwordId");
+var userName = document.querySelector("#userNameId");
 var form = document.querySelector("#form");
 
+form.addEventListener("submit", loginHandle);
 
-form.addEventListener("submit",loginHandle);
 
-function initalLoad() {
-    let userData = localStorage.getItem("userData");
-      
-    if (!userData) {
-      let storage = [];
-      localStorage.setItem("userData", JSON.stringify(storage));
-        }
-      }
+function loginHandle(event) {
+  event.preventDefault();
 
-initalLoad();
-
-function loginHandle(event){
-    event.preventDefault();
-
-    var getData = JSON.parse(localStorage.getItem("data"));
-    console.log(getData)
-    var isExits = getData.find(function (user) {
-
-    return user.email==email.value && user.password==password.value 
-          });
-    if(isExits){
-        location.replace("app.html")
-            return;
-        }
-    else{
-    alert("user not Exits Go to signup page")
-    }
-    }
+  var getData = JSON.parse(localStorage.getItem("data"));
+  console.log(getData);
+  var isExits = getData.find(function (user) {
+    return user.email == email.value && user.password == password.value;
+  });
+  
+  if (isExits) {
+    var userInfo = {
+      email: email.value,
+      userName:userName.value,
+      password: password.value
+  
+    };
+    localStorage.setItem("loginUser", JSON.stringify(userInfo));
+    location.assign("app.html");
+    return;
+  } else {
+    alert("user not Exits Go to signup page");
+  }
+}
